186d390b5dc4a4b1578927987b2fac0ab01c9022
Julie Fox <juliefoxp@gmail.com>
2025-01-31 10:26:59 +0100

    product færdig

--- single.js
+++ single.js
@@ -1,5 +1,5 @@
 const productContainer = document.querySelector(".productContainer");
-const productId = 1530;
+const productId = 1163;
 fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
   .then((response) => response.json())
   .then((data) => {
@@ -7,11 +7,11 @@ fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
     <h1>PRODUCT INFORMATION</h1>
       <div class="productContainer">
         <div class="product-image">
-       <img src="https://kea-alt-del.dk/t7/images/webp/640/1558/${data.productId}.webp"
-        alt="light blue water bottle"/>
+       <img src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp"
+        alt="light blue water bottle" />
         </div>
         <div class="product-details">
-          <h1>WATER BOTTLE ${data.productdisplayname}</h1>
+          <h1> ${data.productdisplayname}</h1>
           <p>Pris: ${data.price}</p> 
           <p>Brand: ${data.category}</p>
           <p>Product details: Light blue, 500 ml vandflaske.</p>
