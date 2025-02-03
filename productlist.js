88232207852ef8e09b3de044469d2b68b4be4e43
Julie Fox <juliefoxp@gmail.com>
2025-02-03 15:15:32 +0100

    add category list

--- productlist.js
+++ productlist.js
@@ -2,6 +2,7 @@ const product_list_container = document.querySelector(".product_list_container")
 fetch(`https://kea-alt-del.dk/t7/api/products/`)
   .then((response) => response.json())
   .then((data) => showList(data));
+
 function showList(products) {
   console.log(products);
   let markup = "";
