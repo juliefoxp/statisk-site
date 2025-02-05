c572845bdf5686797be1444886cb252ee18b8ba8
Julie Fox <juliefoxp@gmail.com>
2025-02-05 16:53:47 +0100

    add old price

--- productlist.js
+++ productlist.js
@@ -18,9 +18,11 @@ function showProducts(data) {
         `
       <div class="card_1 ${element.soldout ? "sold-out" : ""}">
   ${element.soldout ? '<span class="sold-out-label">SOLD OUT</span>' : ""}
+   ${element.discount && element.discount > 0 ? '<span class="discount-label">-' + element.discount + "%</span>" : ""}
           <img src="https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp" alt="acc_1" /> 
           <h1>${element.productdisplayname}</h1>
           <p class="price"> Price: ${element.price}</p>
+          <span class="original-price">Old Price: ${Math.floor(element.price / (1 - element.discount / 100))}</span>
           <p>Productyear: ${element.productionyear}</p>
           <p>Brand: ${element.brandname}</p>
           <p><a href="product.html?id=${element.id}" class="button">ADD TO BASKET</a></p>
